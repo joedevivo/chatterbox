48f05433d2642b6db5a8cd0ab946172f963a5353
Tony Han <h.bing612@gmail.com>
2017-07-22 19:40:49 +0800

    Fix: ignore unknown settings frame

--- src/h2_frame_settings.erl
+++ src/h2_frame_settings.erl
@@ -102,6 +102,8 @@ parse_settings(<<0,5,Val:4/binary,T/binary>>, S) ->
     parse_settings(T, [{?SETTINGS_MAX_FRAME_SIZE, binary:decode_unsigned(Val)}|S]);
 parse_settings(<<0,6,Val:4/binary,T/binary>>, S)->
     parse_settings(T, [{?SETTINGS_MAX_HEADER_LIST_SIZE, binary:decode_unsigned(Val)}|S]);
+parse_settings(<<_:6/binary,T/binary>>, S)->
+    parse_settings(T, S);
 parse_settings(<<>>, Settings) ->
     Settings.
 
